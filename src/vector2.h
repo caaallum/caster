18609c951edc77118a94f16a7239dbe72a6345dc
caaallum <callum_speirs@protonmail.com>
2023-07-11 22:52:49 +0100

    Update vector2.h adding extra constructors

--- src/vector2.h
+++ src/vector2.h
@@ -12,6 +12,9 @@ public:
 
     template<typename U>
     Vector2(U x, U y) : x(static_cast<T>(x)), y(static_cast<T>(y)) {}
+
+    template<typename U>
+    Vector2(Vector2<U> v) : x(static_cast<T>(v.x)), y(static_cast<T>(v.y)) {}
 };
 
 #endif /* __VECTOR2_H */
